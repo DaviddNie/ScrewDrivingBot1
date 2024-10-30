eaea27b6b6ea8ea1e9c6b044b662c2a2ef312f36
David Nie <davidnie0418@gmail.com>
2024-10-30 15:02:15 +1100

    add test_vision launch

---
+++ src/brain/launch/test_vision_launch.py
@@ -0,0 +1,37 @@
+import launch
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+import os
+
+def generate_launch_description():
+
+    os.environ["XDG_SESSION_TYPE"] = "xcb"
+    
+    brain_node = Node(
+            package='brain',
+            executable='brain',  # Replace with the actual executable name
+            name='brain',
+            output='screen',
+            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+        )
+
+    vision = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                get_package_share_directory('vision'), 'launch'),
+                '/vision_launch.py'])
+            )
+
+    return LaunchDescription([
+        # Declare launch arguments (optional)
+        DeclareLaunchArgument('use_sim_time', default_value='false', 
+                               description='Use simulation time if true'),
+
+        brain_node,
+        vision,
+    ])
