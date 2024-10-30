f1fffad7736f1906affaf28f5f5edfc9f322d7f8
David Nie <davidnie0418@gmail.com>
2024-10-30 15:45:49 +1100

    set up ros bag playback in test_vision_launch

--- src/brain/launch/test_vision_launch.py
+++ src/brain/launch/test_vision_launch.py
@@ -1,8 +1,8 @@
 import launch
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
@@ -11,27 +11,35 @@ import os
 
 def generate_launch_description():
 
-    os.environ["XDG_SESSION_TYPE"] = "xcb"
-    
-    brain_node = Node(
-            package='brain',
-            executable='brain',  # Replace with the actual executable name
-            name='brain',
-            output='screen',
-            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
-        )
+	os.environ["XDG_SESSION_TYPE"] = "xcb"
+	
+	bag_file_path = '/home/davidnie/4231/ScrewDrivingBot1/camera_bag'
 
-    vision = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([os.path.join(
-                get_package_share_directory('vision'), 'launch'),
-                '/vision_launch.py'])
-            )
+	# Play the bag file using ExecuteProcess
+	play_bag = ExecuteProcess(
+		cmd=['ros2', 'bag', 'play', bag_file_path, '--clock'],
+		output='screen'
+	)
+	
+	brain_node = Node(
+			package='brain',
+			executable='brain',  # Replace with the actual executable name
+			name='brain',
+			output='screen',
+			parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+		)
 
-    return LaunchDescription([
-        # Declare launch arguments (optional)
-        DeclareLaunchArgument('use_sim_time', default_value='false', 
-                               description='Use simulation time if true'),
+	vision = IncludeLaunchDescription(
+			PythonLaunchDescriptionSource([os.path.join(
+				get_package_share_directory('vision'), 'launch'),
+				'/vision_launch.py'])
+			)
 
-        brain_node,
-        vision,
-    ])
+	return LaunchDescription([
+		# Declare launch arguments (optional)
+		DeclareLaunchArgument('use_sim_time', default_value='false', 
+							   description='Use simulation time if true'),
+		play_bag,
+		brain_node,
+		vision,        
+	])
